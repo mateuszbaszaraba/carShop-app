e18430ad415243acfa8e915c3ffeb61fd9e2d151
Matt <mateuszbaszaraba@outlook.com>
2020-05-20 20:50:51 +0200

    done displaying car

---
+++ src/components/cars/Car.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="row row-cols-1 row-cols-md-2">
+  <div class="col mb-4">
+    <div class="card" style="margin-top: 2vh">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+</div>
+</template>
+
+<script>
+    export default {
+        props: ['car']
+    }
+</script>
---
+++ src/components/cars/CarGrid.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <app-car v-for="car in cars" :key="car.id" :car="car"></app-car>
+    </div>
+</template>
+
+<script>
+    import car from './Car';
+
+    export default {
+        data() {
+            return {
+                cars: [
+                    { id: 1, brand: 'BMW', price: '50'},
+                    { id: 2, brand: 'Mercedes', price: '55'},
+                    { id: 3, brand: 'VW', price: '20'},
+                    { id: 4, brand: 'Audi', price: '40'}
+                ]
+            }
+        },
+        components: {
+            appCar: car
+        }
+    }
+</script>
